bb4294836100211a35f7097cdf0d4782a3a76709
vvs0503624459 <106775038+vvs0503624459@users.noreply.github.com>
2023-12-23 21:11:49 +0200

    changed icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,34 @@
 import "./App.scss";
-import { lazy } from "react";
+import { lazy, useEffect } from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import SignupPage from "./pages/SignupPage/SignupPage";
 import SigninPage from "./pages/SigninPage/SigninPage";
+import { useAppDispatch } from "./redux/hooks";
+import { getMainProduct } from "./redux/products/products-operation";
 // import { Home } from "./pages/Home/Home";
 // import ProductCard from "./pages/ProductCard/ProductCard";
 
 const Home = lazy(() => import("./pages/Home/Home"));
 const ProductCard = lazy(() => import("./pages/ProductPage/ProductPage"));
 const General = lazy(() => import("./components/ProductCard/General/General"));
-const Specifications = lazy(() =>
-  import("./components/ProductCard/Specifications/Specifications")
+const Specifications = lazy(
+  () => import("./components/ProductCard/Specifications/Specifications")
 );
-const ReviewsQuestion = lazy(() =>
-  import("./components/ProductCard/ReviewsQuestion/ReviewsQuestion")
+const ReviewsQuestion = lazy(
+  () => import("./components/ProductCard/ReviewsQuestion/ReviewsQuestion")
 );
-const Accessories = lazy(() =>
-  import("./components/ProductCard/Accessories/Accessories")
+const Accessories = lazy(
+  () => import("./components/ProductCard/Accessories/Accessories")
 );
 
 // import {GlobalStyles} from './components/globalStyles';
 
 function App() {
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    dispatch(getMainProduct());
+  }, []);
   return (
     <>
       <Routes>
